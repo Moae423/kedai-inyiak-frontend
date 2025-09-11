b0cc3663204628310bc87210988dbca45015fa4e
Daffa Rihadatul Fairus <hanura423@gmail.com>
2025-09-11 17:41:29 +0700

    feat: add CardTotal component and implement useTotalUser hook for user data retrieval

---
+++ src/components/dashboard/cardTotal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { GoPackage } from "react-icons/go";
+
+interface CardTotalProps {
+  loading?: boolean;
+  data?: number;
+  title: string;
+  error?: string | null;
+}
+const CardTotal = ({ loading, data, error, title }: CardTotalProps) => {
+  return (
+    <div className="w-[25rem] h-full bg-gray-50 shadow-md border-2 p-4 rounded-2xl">
+      <div className="flex items-center gap-3">
+        <GoPackage className="w-12 h-12" />
+        <div className="flex flex-col">
+          <h1 className="text-[24px] leading-[32px] font-bold ">
+            {loading ? "Loading..." : error ? error : data}
+          </h1>
+          <p className="text-[18px] leadieng-[24px] text-gray-500">{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardTotal;
